99de90d7117435945fe059782893d81b180cf139
Nguyễn Ngọc Vỹ <22130332@st.hcmuaf.edu.vn>
2025-01-05 09:07:02 +0700

    Viet class OrderServices va sevlet xoa don hang

---
+++ src/main/java/vn/edu/hcmuaf/fit/projectwebck/services/OrderServices.java
@@ -0,0 +1,27 @@
+package vn.edu.hcmuaf.fit.projectwebck.services;
+
+
+
+import vn.edu.hcmuaf.fit.projectwebck.dao.OrderDao;
+import vn.edu.hcmuaf.fit.projectwebck.dao.model.Order;
+import vn.edu.hcmuaf.fit.projectwebck.dao.model.OrderDetail;
+
+import java.util.List;
+
+public class OrderServices {
+    static OrderDao orderDao = new OrderDao();
+
+    public List<Order> getAllOrders() {
+        return orderDao.getAllOrders();
+    }
+
+    // Lấy đơn hàng theo ID
+    public List<OrderDetail> getOrderDetailsByOrderId(int orderId) {
+        return orderDao.getOrderDetailsByOrderId(orderId);
+    }
+
+    // Xóa một đơn hàng theo ID
+    public void removeOrder(int orderId) {
+        orderDao.removeOrder(orderId);
+    }
+}
