73cf7a00d896326d0bd0fd668c700f18592daf7c
Nguyễn Ngọc Vỹ <22130332@st.hcmuaf.edu.vn>
2025-01-08 22:08:56 +0700

    Viet severlet showCustomerPage

---
+++ src/main/java/vn/edu/hcmuaf/fit/projectwebck/controller/Customer/ShowOptionCustomer.java
@@ -0,0 +1,28 @@
+package vn.edu.hcmuaf.fit.projectwebck.controller.Customer;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import vn.edu.hcmuaf.fit.projectwebck.dao.model.User;
+import vn.edu.hcmuaf.fit.projectwebck.services.UserServices;
+
+import java.io.IOException;
+
+@WebServlet(name = "ShowCustomerPage", value = "/showCustomerPage")
+public class ShowOptionCustomer extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserServices userServices = new UserServices();
+        User user = userServices.getUserById(1);
+        request.setAttribute("user", user);
+        request.getRequestDispatcher("Customer.jsp?runScript=option1").forward(request,response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
