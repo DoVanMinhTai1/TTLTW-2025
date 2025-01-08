ddd0797e8d59371c222ce6c5b01216557343d693
Nguyễn Ngọc Vỹ <22130332@st.hcmuaf.edu.vn>
2025-01-08 22:00:56 +0700

    Viet severlet addAddress

---
+++ src/main/java/vn/edu/hcmuaf/fit/projectwebck/controller/Customer/AddAddress.java
@@ -0,0 +1,54 @@
+package vn.edu.hcmuaf.fit.projectwebck.controller.Customer;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import vn.edu.hcmuaf.fit.projectwebck.dao.model.Address;
+import vn.edu.hcmuaf.fit.projectwebck.services.AddressServices;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "AddAddress", value = "/addAddress")
+public class AddAddress extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("Name");
+        String phone = request.getParameter("Phone");
+        String company = request.getParameter("Company");
+        String address = request.getParameter("Address");
+        String nation = request.getParameter("Nation");
+        String province = request.getParameter("Province");
+        String district = request.getParameter("District");
+        String wardAndCommune = request.getParameter("Wardandcommune");
+        int origin = Integer.parseInt(request.getParameter("isDefault"));
+        String addresss = address + "," + wardAndCommune + "," + district + "," + province + "," + nation;
+        // Kiểm tra trạng thái của nút "Đặt địa chỉ làm mặc định"
+
+        // Tạo đối tượng Address và gán giá trị
+        Address newAddress = new Address();
+        newAddress.setName(name);
+        newAddress.setPhone(phone);
+        newAddress.setCompany(company);
+        newAddress.setAddress(addresss);
+        newAddress.setOrigin(origin);
+        newAddress.setUserId(1);
+
+        // Gọi service để thêm địa chỉ vào cơ sở dữ liệu
+        AddressServices addressServices = new AddressServices();
+        addressServices.insertAddress(newAddress);
+        List<Address> listAddress = addressServices.getAllById(1);
+        request.setAttribute("listAddress", listAddress);
+
+        // Forward về trang quản lý địa chỉ
+        request.getRequestDispatcher("Customer.jsp?runScript=option4").forward(request, response);
+
+    }
+}
