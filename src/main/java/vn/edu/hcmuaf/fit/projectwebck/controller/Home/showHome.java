df4f300542ecc3a7be0f13aefc99c686f030a7d1
Trần Nguyễn Thu Ngân <22130180@st.hcmuaf.edu.vn>
2025-01-13 19:05:54 +0700

    update showHome about session

--- src/main/java/vn/edu/hcmuaf/fit/projectwebck/controller/Home/showHome.java
+++ src/main/java/vn/edu/hcmuaf/fit/projectwebck/controller/Home/showHome.java
@@ -3,7 +3,9 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import vn.edu.hcmuaf.fit.projectwebck.dao.model.Product;
+import vn.edu.hcmuaf.fit.projectwebck.dao.model.User;
 import vn.edu.hcmuaf.fit.projectwebck.services.ProductServices;
+import vn.edu.hcmuaf.fit.projectwebck.services.UserServices;
 
 import java.io.IOException;
 import java.util.List;
@@ -18,12 +20,20 @@ public class showHome extends HttpServlet {
 
         List<Product> products = productService.getAllHome();
 
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        request.setAttribute("logInUser", user);
+
         request.setAttribute("listProductBestSeller",productsSeller);
         request.setAttribute("allProduct",products);
         request.getRequestDispatcher("jsp/Home.jsp").forward(request,response);
+
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
     }
 }
