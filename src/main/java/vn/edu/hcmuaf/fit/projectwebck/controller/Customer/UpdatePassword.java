450f9e5489d4e4d042a04e6c5a68bfb33292b11f
Trần Nguyễn Thu Ngân <22130180@st.hcmuaf.edu.vn>
2025-01-15 11:34:16 +0700

    method hashPassword in updatePassword servlet

--- src/main/java/vn/edu/hcmuaf/fit/projectwebck/controller/Customer/UpdatePassword.java
+++ src/main/java/vn/edu/hcmuaf/fit/projectwebck/controller/Customer/UpdatePassword.java
@@ -5,10 +5,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import vn.edu.hcmuaf.fit.projectwebck.dao.model.User;
 import vn.edu.hcmuaf.fit.projectwebck.services.UserServices;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @WebServlet(name = "UpdatePassword", value = "/updatePassword")
 public class UpdatePassword extends HttpServlet {
@@ -35,9 +38,15 @@ public class UpdatePassword extends HttpServlet {
 
         // Kiểm tra mật khẩu cũ
         UserServices userServices = new UserServices();
-        User user = userServices.getUserById(1);
+        User user = userServices.getUserById(8);
         String storedPassword = user.getPassword();
 
+        String password = request.getParameter("password");
+
+        String hashedPassword = hashPassword(password);
+        if (hashedPassword.equals(storedPassword)) {
+            
+        }
 
         if (!oldPassword.equals(storedPassword)) {
             request.setAttribute("error", "Mật khẩu cũ không đúng.");
@@ -50,5 +59,24 @@ public class UpdatePassword extends HttpServlet {
         // Thông báo thành công
         request.setAttribute("success", "Mật khẩu đã được thay đổi thành công!");
         request.getRequestDispatcher("Customer.jsp?runScript=option3").forward(request, response);
+    }
+
+    private String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(password.getBytes());
+            byte[] digest = md.digest();
+
+            // Chuyển đổi mảng byte thành chuỗi hex
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : digest) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
