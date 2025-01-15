30c993afdf0019ce8a21ee28f9bca67b94e240c5
Trần Nguyễn Thu Ngân <22130180@st.hcmuaf.edu.vn>
2025-01-15 11:33:27 +0700

    paging, sort in Vegetables.jsp

--- src/main/java/vn/edu/hcmuaf/fit/projectwebck/controller/Home/Vegetables/showVegetables.java
+++ src/main/java/vn/edu/hcmuaf/fit/projectwebck/controller/Home/Vegetables/showVegetables.java
@@ -15,6 +15,18 @@ public class showVegetables extends  HttpServlet{
         ProductServices productService = new ProductServices();
         List<Product> vegetables = productService.getAllVegetables();
 
+        if (request.getAttribute("listVegetables") != null) {
+            vegetables = (List<Product>) request.getAttribute("listVegetables");
+        } else {
+            vegetables = productService.getAllVegetables(); // Lấy danh sách sản phẩm từ database
+        }
+
+        int count = productService.getTotalVegetables();
+        int productsPerPage = 50; // Số sản phẩm trên mỗi trang
+        int endPage = (int) Math.ceil((double) count / productsPerPage);
+
+        request.setAttribute("endPage", endPage);
+
         request.setAttribute("listVegetables",vegetables);
         request.getRequestDispatcher("jsp/Vegetables.jsp").forward(request,response);
     }
