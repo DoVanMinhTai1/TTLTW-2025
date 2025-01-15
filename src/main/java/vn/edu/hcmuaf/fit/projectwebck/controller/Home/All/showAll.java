33fee4b13fc27fe2ace9d7e6aad71996a907fdb9
Trần Nguyễn Thu Ngân <22130180@st.hcmuaf.edu.vn>
2025-01-15 11:33:06 +0700

    paging and sort in all.jsp

--- src/main/java/vn/edu/hcmuaf/fit/projectwebck/controller/Home/All/showAll.java
+++ src/main/java/vn/edu/hcmuaf/fit/projectwebck/controller/Home/All/showAll.java
@@ -6,6 +6,7 @@ import vn.edu.hcmuaf.fit.projectwebck.dao.model.Product;
 import vn.edu.hcmuaf.fit.projectwebck.services.ProductServices;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "showAll", value = "/showAll")
@@ -13,9 +14,38 @@ public class showAll extends  HttpServlet{
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ProductServices productService = new ProductServices();
-        List<Product> products = productService.getAll();
+        String indexPage = request.getParameter("index");
 
-        request.setAttribute("listProduct", products);
+        if (indexPage == null) {
+            indexPage = "1"; // Mặc định chỉ số trang là 1
+        }
+        int index = Integer.parseInt(indexPage);
+
+        // Lấy tổng số sản phẩm
+        int count = productService.getTotalProducts();
+        int productsPerPage = 50; // Số sản phẩm trên mỗi trang
+        int endPage = (int) Math.ceil((double) count / productsPerPage);
+
+        // Lấy danh sách sản phẩm đã được sắp xếp từ servlet sort
+        List<Product> products;
+        if (request.getAttribute("listPaging") != null) {
+            products = (List<Product>) request.getAttribute("listPaging");
+        } else {
+            products = productService.getAll(); // Nếu không có, lấy tất cả sản phẩm
+        }
+
+        // Phân trang
+        int fromIndex = (index - 1) * productsPerPage;
+        int toIndex = Math.min(fromIndex + productsPerPage, products.size());
+        List<Product> list = products.subList(fromIndex, toIndex);
+
+        // Lưu giá trị sortProduct về request để hiển thị trên giao diện
+        String sortProduct = request.getParameter("sortProduct");
+        request.setAttribute("sortProduct", sortProduct);
+
+        request.setAttribute("listPaging", list);
+        request.setAttribute("endPage", endPage);
+//        request.setAttribute("listProduct", products);
         request.getRequestDispatcher("jsp/All.jsp").forward(request,response);
     }
     @Override
