90fd1369cc1b1552ec4f4d8f57f57ac6e79df454
Trần Nguyễn Thu Ngân <22130180@st.hcmuaf.edu.vn>
2025-01-15 11:28:49 +0700

    sortFruits

---
+++ src/main/java/vn/edu/hcmuaf/fit/projectwebck/controller/Home/Fruits/sortFruits.java
@@ -0,0 +1,51 @@
+package vn.edu.hcmuaf.fit.projectwebck.controller.Home.Fruits;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import vn.edu.hcmuaf.fit.projectwebck.dao.model.Product;
+import vn.edu.hcmuaf.fit.projectwebck.services.ProductServices;
+
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+@WebServlet(name = "sortFruits", value = "/sortFruits")
+public class sortFruits extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String sort = request.getParameter("sortProduct");
+        ProductServices productService = new ProductServices();
+        List<Product> products = productService.getAllFruits();
+
+        if ("Giá giảm dần".equals(sort)) {
+            // Sắp xếp sản phẩm theo giá giảm dần
+            Collections.sort(products, new Comparator<Product>() {
+                @Override
+                public int compare(Product p1, Product p2) {
+                    return Double.compare(p2.getPrice(), p1.getPrice());
+                }
+            });
+        } else if ("Giá tăng dần".equals(sort)) {
+            // Sắp xếp sản phẩm theo giá tăng dần
+            Collections.sort(products, new Comparator<Product>() {
+                @Override
+                public int compare(Product p1, Product p2) {
+                    return Double.compare(p1.getPrice(), p2.getPrice());
+                }
+            });
+        } else {
+            // Sắp xếp mặc định
+
+        }
+        request.setAttribute("listFruits", products);
+        request.getRequestDispatcher("showFruits").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
