d97712935d8995f63b5493f640698b3f13733a28
Trần Nguyễn Thu Ngân <22130180@st.hcmuaf.edu.vn>
2025-03-06 16:48:41 +0700

    first commit

---
+++ src/main/java/vn/edu/hcmuaf/fit/projectwebck/controller/Home/search.java
@@ -0,0 +1,33 @@
+package vn.edu.hcmuaf.fit.projectwebck.controller.Home;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import vn.edu.hcmuaf.fit.projectwebck.dao.model.Product;
+import vn.edu.hcmuaf.fit.projectwebck.services.ProductServices;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "searchProduct", value = "/searchProduct")
+public class searchProduct extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String keyword = request.getParameter("name"); // Lấy từ khóa tìm kiếm từ request
+        ProductServices productServices = new ProductServices();
+
+        // Tìm kiếm sản phẩm theo tên
+        List<Product> searchedProducts = productServices.searchByName(keyword);
+
+        // Đặt danh sách vào request và chuyển đến trang JSP
+        request.setAttribute("listproduct", searchedProducts);
+
+        request.getRequestDispatcher("jsp/Search.jsp").forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
