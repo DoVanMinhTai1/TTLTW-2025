580951a9953297b84b0e3ed1822a6a9d31af3245
Trần Nguyễn Thu Ngân <22130180@st.hcmuaf.edu.vn>
2025-03-06 16:48:41 +0700

    first commit

---
+++ src/main/java/vn/edu/hcmuaf/fit/projectwebck/controller/SingInUp/sendPassword.java
@@ -0,0 +1,10 @@
+package vn.edu.hcmuaf.fit.projectwebck.controller.SingInUp; import jakarta.servlet.*; 
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException; 
+@WebServlet(name = "sendPassword", value = "/sendPassword") 
+public class sendPassword extends HttpServlet { 
+
+@Override protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { } 
+@Override protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { } 
+}
