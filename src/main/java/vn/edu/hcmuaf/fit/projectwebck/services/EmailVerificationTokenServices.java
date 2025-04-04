2581324b0e58c1de4e986e28be18aa2e7da40542
Trần Nguyễn Thu Ngân <22130180@st.hcmuaf.edu.vn>
2025-04-04 23:52:35 +0700

    create emailVerificationTokenServices

---
+++ src/main/java/vn/edu/hcmuaf/fit/projectwebck/services/EmailVerificationTokenServices.java
@@ -0,0 +1,16 @@
+package vn.edu.hcmuaf.fit.projectwebck.services;
+
+
+
+import vn.edu.hcmuaf.fit.projectwebck.dao.EmailVerificationTokenDao;
+import vn.edu.hcmuaf.fit.projectwebck.dao.model.EmailVerificationToken;
+
+import java.util.List;
+
+public class EmailVerificationTokenServices {
+    EmailVerificationTokenDao emailVerificationTokenDao = new EmailVerificationTokenDao();
+
+    public void insertToken(EmailVerificationToken token){emailVerificationTokenDao.insertToken(token);}
+    public EmailVerificationToken findToken(String token){return emailVerificationTokenDao.findToken(token);}
+    public void markAsUsed(String token) {emailVerificationTokenDao.markAsUsed(token);}
+}
