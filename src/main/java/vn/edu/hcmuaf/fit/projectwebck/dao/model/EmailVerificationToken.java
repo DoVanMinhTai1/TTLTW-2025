fae940a29aa3f0a774d4fe4e938bff0056327073
Trần Nguyễn Thu Ngân <22130180@st.hcmuaf.edu.vn>
2025-04-04 23:52:14 +0700

    update emailVerificationToken.java

--- src/main/java/vn/edu/hcmuaf/fit/projectwebck/dao/model/EmailVerificationToken.java
+++ src/main/java/vn/edu/hcmuaf/fit/projectwebck/dao/model/EmailVerificationToken.java
@@ -14,7 +14,40 @@ public class EmailVerificationToken {
         this.email = email;
     }
 
-
-
-
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+    public void setExpiresAt(LocalDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+    public boolean isUsed() {
+        return isUsed;
+    }
+    public void setUsed(boolean used) {
+        isUsed = used;
+    }
 }
