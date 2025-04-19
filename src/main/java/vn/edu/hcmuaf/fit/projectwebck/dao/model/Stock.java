088f361591e3f7fec2bd454686bac3c8cf1eea7b
dovanminhtai1 <22130238@st.hcmuaf.edu.vn>
2025-04-19 21:16:17 +0700

    CRUD Table Stock

--- src/main/java/vn/edu/hcmuaf/fit/projectwebck/dao/model/Stock.java
+++ src/main/java/vn/edu/hcmuaf/fit/projectwebck/dao/model/Stock.java
@@ -1,6 +1,7 @@
 package vn.edu.hcmuaf.fit.projectwebck.dao.model;
 
 public class Stock {
+    private int id;
     private int productId;
     private int quantity;
     private String name;
@@ -9,7 +10,8 @@ public class Stock {
     private String stateOrProvince;
     private String  country;
 
-    public Stock(int productId, int quantity, String name, String addressLine, String district, String stateOrProvince, String country) {
+    public Stock(int id,int productId, int quantity, String name, String addressLine, String district, String stateOrProvince, String country) {
+        this.id = id;
         this.productId = productId;
         this.quantity = quantity;
         this.name = name;
@@ -20,5 +22,85 @@ public class Stock {
     }
 
     public Stock() {
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddressLine() {
+        return addressLine;
+    }
+
+    public void setAddressLine(String addressLine) {
+        this.addressLine = addressLine;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getStateOrProvince() {
+        return stateOrProvince;
+    }
+
+    public void setStateOrProvince(String stateOrProvince) {
+        this.stateOrProvince = stateOrProvince;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "id=" + id +
+                ", productId=" + productId +
+                ", quantity=" + quantity +
+                ", name='" + name + '\'' +
+                ", addressLine='" + addressLine + '\'' +
+                ", district='" + district + '\'' +
+                ", stateOrProvince='" + stateOrProvince + '\'' +
+                ", country='" + country + '\'' +
+                '}';
     }
 }
