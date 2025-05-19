7dff0ba7c6b812224198fdca70dac9acc1b76e6e
Trần Nguyễn Thu Ngân <22130180@st.hcmuaf.edu.vn>
2025-05-19 23:18:11 +0700

    create role.java

---
+++ src/main/java/vn/edu/hcmuaf/fit/projectwebck/dao/model/Role.java
@@ -0,0 +1,12 @@
+package vn.edu.hcmuaf.fit.projectwebck.dao.model;
+
+public class Role {
+    public static final int USER = 0;
+    public static final int ADMIN = 1;
+    public static final int MOD_VEGETABLES = 2;
+    public static final int MOD_USERS = 3;
+    public static final int MOD_ORDERS = 4;
+    public static final int MOD_PROMOTIONS = 5;
+    public static final int MOD_PRODUCT_PROMOTION = 6;
+    public static final int GUEST = 7;
+}
